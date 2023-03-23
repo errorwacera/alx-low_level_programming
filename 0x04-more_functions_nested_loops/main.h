7673b16862a9bc0c14e67b992ea48e0cccf0cc37
errorwacera <joywachera1@gmail.com>
2023-03-23 15:32:43 -0700

    solutios

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
+
+#endif /* MAIN_H */
