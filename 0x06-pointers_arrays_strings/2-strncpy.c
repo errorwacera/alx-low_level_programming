5ddf085dfd03b118f25499aebce61b5d98b6553f
errorwacera <joywachera1@gmail.com>
2023-03-30 11:27:51 -0700

    corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,31 +1,36 @@
 /**
- * _strncat - a function that concatenates two strings.
+ * _strncpy - A function that copies a string.
  *
- * @dest: pointer to destination input
- * @src: pointer to source input
- * @n: most number of bytes from @src
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
  *
  * Return: @dest
 */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int c, i;
-
-	c = 0;
-
-	/*find size of dest array*/
-	while (dest[c])
-		c++;
+	int i;
 
 	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
 	*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/*null terminate dest*/
-	dest[c + i] = '\0';
+		dest[i] = src[i];
+	/**
+	 * if the length of source is less than n
+	 * write additional nullbytes to dest to
+	 * ensure that a total of n bytes is written
+	*/
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
